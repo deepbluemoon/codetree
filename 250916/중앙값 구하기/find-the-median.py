428b95d477243c8e5a2830e585daa1766527b8d0
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-09-16 14:18:12 +0900

    Title: 중앙값 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250916/중앙값 구하기/find-the-median.py
@@ -0,0 +1,18 @@
+A, B, C = map(int, input().split())
+
+if A > B:
+    if A < C:
+        print(A)
+    else:
+        if B > C:
+            print(B)
+        else:
+            print(C)
+else:
+    if A > C:
+        print(A)
+    else:
+        if B > C:
+            print(C)
+        else:
+            print(B)
