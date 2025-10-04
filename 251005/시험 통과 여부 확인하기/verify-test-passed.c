72c65148df00835abb3e9348daa8467976973bbf
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 01:24:57 +0900

    Title: 시험 통과 여부 확인하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    
+    scanf("%d", &N);
+
+    if (N >= 80){
+        printf("pass");
+    }
+    else {
+        printf("%d more score", 80 - N);
+    }
+
+    return 0;
+}
