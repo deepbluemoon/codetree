71e960e3dec6392d6aed79dc14eb7ae7e01522d8
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 02:49:57 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/문자 출력하기/print-text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char N[100];
+    int i = 0;
+
+    scanf("%s", N);
+
+    for (i = 0; i < 8; i++){
+        printf("%s", N);
+    }
+    return 0;
+}
