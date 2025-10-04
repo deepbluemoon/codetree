88814d0bb7ade810522a00819ed2a2581ab018e1
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 01:21:33 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/비교 연산/comparison-operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A,B;
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d\n", A >= B);
+    printf("%d\n", A > B);
+    printf("%d\n", A <= B);
+    printf("%d\n", A < B);
+    printf("%d\n", A == B);
+    printf("%d\n", A != B);
+
+    return 0;
+}
