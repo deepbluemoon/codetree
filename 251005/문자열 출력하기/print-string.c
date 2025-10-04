2c1b0a27c36c7291243bd568c8e26374d4fe90c2
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 02:52:35 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/문자열 출력하기/print-string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    int i = 0;
+    scanf("%d", &N);
+
+    for(i = 1; i <= N; i++){
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
