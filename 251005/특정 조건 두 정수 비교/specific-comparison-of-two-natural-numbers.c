e0f587fd1e0880d18c0b55da2385479a660bd738
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 01:31:56 +0900

    Title: 특정 조건 두 정수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    if (A < B){
+        printf("%d ", A < B);
+    }
+    else{
+        printf("%d ", A < B);
+    }
+
+    if (A == B){
+        printf("%d", A == B);
+    }
+    else {
+        printf("%d", A == B);
+    }
+    return 0;
+}
