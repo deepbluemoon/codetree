806f387e280b980862f78479a1134dbedadd4b5a
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 04:50:47 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251006/세 실수의 반올림/rounding-of-three-actual-numbers.c
+++ 251006/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -3,7 +3,7 @@
 int main() {
     // Please write your code here.
     float a, b, c;
-    scanf("%f\n%f\n%f", a,b,c);
+    scanf("%f\n%f\n%f", &a,&b,&c);
     printf("%.3f\n%.3f\n%.3f", a,b,c);
     return 0;
 }
