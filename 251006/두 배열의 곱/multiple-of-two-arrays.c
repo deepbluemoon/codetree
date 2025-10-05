531615914270506bd32fc3cecf8c6ff2a378d458
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 01:09:09 +0900

    Title: 두 배열의 곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/두 배열의 곱/multiple-of-two-arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int matrix1[3][3];
+    int matrix2[3][3];
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            printf("%d ", matrix1[i][j] * matrix2[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
