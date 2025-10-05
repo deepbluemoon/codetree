4ce588ec19cf863628d155f6391dbd15115ccb0c
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 02:39:59 +0900

    Title: 특정 원소들의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/특정 원소들의 합/sum-of-specific-elements.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr[4][4];
+    int cnt = 0;
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < i + 1; j++){
+            cnt += arr[i][j];
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
