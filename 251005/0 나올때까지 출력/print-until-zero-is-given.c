23f8e95fda18f53ad9db1e3ab59ace955daea980
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 19:37:33 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Please write your code here.
+    
+    
+    while (1){
+        int N;
+        scanf("%d", &N);
+        if (N == 0){
+            break;
+        }
+        else{
+            printf("%d\n", N);
+        }
+    }
+
+    return 0;
+}
