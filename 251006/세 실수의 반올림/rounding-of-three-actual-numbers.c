5eafa46649ac8e22bc03840ae6c80bf89dd53ae9
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 04:50:22 +0900

    Title: 세 실수의 반올림, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251006/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a, b, c;
+    scanf("%f\n%f\n%f", a,b,c);
+    printf("%.3f\n%.3f\n%.3f", a,b,c);
+    return 0;
+}
