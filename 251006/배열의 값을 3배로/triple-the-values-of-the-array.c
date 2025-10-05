3b272af3b1c68f9ff4700524960602f38c6395f1
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 01:02:04 +0900

    Title: 배열의 값을 3배로, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/배열의 값을 3배로/triple-the-values-of-the-array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int matrix[3][3];
+    int new_matrix[3][3];
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+
+    }
+
+    for (int x = 0; x < 3; x++){
+        for (int y = 0; y < 3; y++){
+            printf("%d ", matrix[x][y]*3);
+        }
+        printf("\n");
+    }
+    return 0;
+}
