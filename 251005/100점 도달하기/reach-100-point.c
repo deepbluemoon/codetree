c5bb9a8a0db190607f13a8ce56e4480c06cd60b5
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 15:09:43 +0900

    Title: 100점 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/100점 도달하기/reach-100-point.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+
+    for (int i = N; i < 101; i++){
+        if (i >= 90){
+            printf("A ");
+        }
+        else if (i >= 80){
+            printf("B ");
+        }
+        else if (i >= 70){
+            printf("C ");
+        }
+        else if (i >= 60){
+            printf("D ");
+        }
+        else{
+            printf("F ");
+        }
+    }
+
+    return 0;
+}
