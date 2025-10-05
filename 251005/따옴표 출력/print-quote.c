622b4b3eecc56b4c71c8ba792967f43d2bfbdae4
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-05 12:09:51 +0900

    Title: 따옴표 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/따옴표 출력/print-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char sentence[] = "He says \"It\'s a really simple sentence\".";
+    printf("%s", sentence);
+    return 0;
+}
