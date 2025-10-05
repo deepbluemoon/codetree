4d8316aed4d58823f2c2fdb07a13d398be6d20bd
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 03:42:23 +0900

    Title: 길이 단위 변환하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float ft = 30.48;
+    int mi = 160934;
+
+    printf("%.1lfft = %.1lfcm\n", 9.2, 9.2 * ft);
+    printf("%.1lfmi = %.1lfcm",1.3, 1.3 * mi );
+    return 0;
+}
