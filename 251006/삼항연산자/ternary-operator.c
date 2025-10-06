65a2df6806f44932564e4e77c895a807e4b058de
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 09:40:16 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/삼항연산자/ternary-operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+
+    scanf("%d", &a);
+
+    printf((a == 100) ? "pass\n" : "failure\n");
+
+
+    return 0;
+}
