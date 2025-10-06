4d25ddd48d26f94d14cf1ab1241713c6a6c5fad1
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-06 14:36:11 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b, c, d, e;
+
+    scanf("%d\n%d %d %d %d", &a, &b, &c, &d, &e);
+
+    printf("%d\n", a > b);
+    printf("%d\n", a > c);
+    printf("%d\n", a > d);
+    printf("%d\n", a > e);
+    return 0;
+}
