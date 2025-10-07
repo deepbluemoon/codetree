bd87a826a9fb84899892bb4414754776e235b40e
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-08 00:46:06 +0900

    Title: 자연수면 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251008/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a > 0){
+        for(int i = 0; i < b; i++){
+            printf("%d", a);
+        }
+    }
+    else{
+        printf("%d", a > 0);
+    }
+    return 0;
+}
