aa38035dd37949f5597615300edda0739c47bf7a
deepbluemoon <105283439+deepbluemoon@users.noreply.github.com>
2025-10-07 20:03:15 +0900

    Title: 별 N개 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/별 N개 출력하기/print-n-stars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n, i = 0;
+    scanf("%d", &n);
+
+    while (i < n){
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
